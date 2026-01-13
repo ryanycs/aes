6c997a547a05affda366215fa2a8d001d6d3821b
ryanycs <ryan.y.c.su@gmail.com>
2026-01-13 16:33:59 +0800

    test: add aes_ctr testcase for AES192, AES256

--- tests/test_aes_ctr/tb.sv
+++ tests/test_aes_ctr/tb.sv
@@ -1,39 +1,58 @@
 `timescale 1ns/10ps
+
+`include "define.svh"
+
 `define CYCLE 10
 `define MAX_CYCLES 1000
-`define NUM_TESTS 4
+`define NUM_TESTS 100
 
 module tb;
 
 // AES CTR port
-logic         clk;
-logic         rst_n;
-logic [127:0] key_i;
-logic         key_valid_i;
-logic         key_ready_o;
-logic [127:0] iv_i;
-logic         iv_valid_i;
-logic [127:0] din_i;
-logic         din_valid_i;
-logic         din_ready_o;
-logic [127:0] dout_o;
-logic         dout_valid_o;
+logic                clk;
+logic                rst_n;
+logic [KEY_SIZE-1:0] key_i;
+logic                key_valid_i;
+logic                key_ready_o;
+logic [127:0]        iv_i;
+logic                iv_valid_i;
+logic [127:0]        din_i;
+logic                din_valid_i;
+logic                din_ready_o;
+logic [127:0]        dout_o;
+logic                dout_valid_o;
 
-logic [127:0] input_key         [0:0];
-logic [127:0] input_iv          [0:0];
-logic [127:0] input_plaintext   [`NUM_TESTS-1:0];
-logic [127:0] golden_ciphertext [`NUM_TESTS-1:0];
+logic [KEY_SIZE-1:0] key        [0:0];
+logic [127:0]        iv         [0:0];
+logic [127:0]        plaintext  [`NUM_TESTS-1:0];
+logic [127:0]        ciphertext [`NUM_TESTS-1:0];
 
 int i = 0;
 int recv = 0;
 int error = 0;
 
+`ifdef AES192
 initial begin
-    $readmemh("tests/test_aes_ctr/key.hex", input_key);
-    $readmemh("tests/test_aes_ctr/iv.hex", input_iv);
-    $readmemh("tests/test_aes_ctr/input.hex", input_plaintext);
-    $readmemh("tests/test_aes_ctr/golden.hex", golden_ciphertext);
+    $readmemh("tests/test_aes_ctr/aes192/key.hex", key);
+    $readmemh("tests/test_aes_ctr/aes192/iv.hex", iv);
+    $readmemh("tests/test_aes_ctr/aes192/plaintext.hex", plaintext);
+    $readmemh("tests/test_aes_ctr/aes192/ciphertext.hex", ciphertext);
 end
+`elsif AES256
+initial begin
+    $readmemh("tests/test_aes_ctr/aes256/key.hex", key);
+    $readmemh("tests/test_aes_ctr/aes256/iv.hex", iv);
+    $readmemh("tests/test_aes_ctr/aes256/plaintext.hex", plaintext);
+    $readmemh("tests/test_aes_ctr/aes256/ciphertext.hex", ciphertext);
+end
+`else
+initial begin
+    $readmemh("tests/test_aes_ctr/aes128/key.hex", key);
+    $readmemh("tests/test_aes_ctr/aes128/iv.hex", iv);
+    $readmemh("tests/test_aes_ctr/aes128/plaintext.hex", plaintext);
+    $readmemh("tests/test_aes_ctr/aes128/ciphertext.hex", ciphertext);
+end
+`endif
 
 aes_ctr dut(
     .clk,
@@ -79,9 +98,9 @@ initial begin
     @(posedge clk);
 
     // Configure key and IV
-    key_i       = input_key[0];
+    key_i       = key[0];
     key_valid_i = 1'b1;
-    iv_i       = input_iv[0];
+    iv_i       = iv[0];
     iv_valid_i = 1'b1;
     @(posedge clk);
     key_valid_i = 1'b0;
@@ -90,13 +109,16 @@ initial begin
     // Feed plaintexts
     wait (din_ready_o == 1'b1);
     while (i < `NUM_TESTS) begin
-        if ($urandom_range(0,1) == 1) begin  // Randomly skip sending input data
-            din_valid_i = 1'b0;
-        end else begin
-            din_valid_i = 1'b1;
-            din_i       = input_plaintext[i];
-            i           = i + 1;
-        end
+        // if ($urandom_range(0,1) == 1) begin  // Randomly skip sending input data
+        //     din_valid_i = 1'b0;
+        // end else begin
+        //     din_valid_i = 1'b1;
+        //     din_i       = plaintext[i];
+        //     i           = i + 1;
+        // end
+        din_valid_i = 1'b1;
+        din_i       = plaintext[i];
+        i           = i + 1;
         @(posedge clk);
     end
 
@@ -106,8 +128,8 @@ end
 // Check outputs
 always @(posedge clk) begin
     if (dout_valid_o) begin
-        if (dout_o !== golden_ciphertext[recv]) begin
-            $display("Mismatch at test %0d: expected %h, got %h", recv, golden_ciphertext[recv], dout_o);
+        if (dout_o !== ciphertext[recv]) begin
+            $display("Mismatch at test %0d: expected %h, got %h", recv, ciphertext[recv], dout_o);
             error = error + 1;
         end
         recv <= recv + 1;
